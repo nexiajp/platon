2f50e74cbc881c600b6587bc3d769d0d3d8a37b1
Se.Okabe <okabe@nexia.jp>
2015-02-10 18:19:31 +0900

    Json Data Pase. Object and string study.

--- test.js
+++ test.js
@@ -4,14 +4,28 @@ var fs      = require('fs');
 var request = require('request');
 
 var Conf = JSON.parse( fs.readFileSync( __dirname + '/.Config.json', 'UTF-8' ) );
+var DataObj = {};
 
 request(Conf.Ping[0].JsonURL, function (err, res, body) {
   if (err || res.statusCode != 200) {
   	errlog("Error: " + err);
   	errlog("Status: %d", res.statusCode);
   }else{
-  	log(body);
+  	//log(body);
+  	DataObj.ec2IPs = JSON.parse(body).ec2IPs;
+  	var obj = {IPs: []};
+  	for(var i in DataObj.ec2IPs){
+  		for(var j in DataObj.ec2IPs[i].EIPs)
+  			obj.IPs.push(DataObj.ec2IPs[i].EIPs[j].PublicIp);
+  	}
+  	log(JSON.stringify(obj, null, "    "));
+  	var ret = IPlistView(obj);
   }
 });
-  
-//log(JSON.stringify(Conf, null, "    "));
+
+function IPlistView(obj){
+	for(var i in obj.IPs){
+		log(obj.IPs[i]);
+	}
+	return true;
+}
