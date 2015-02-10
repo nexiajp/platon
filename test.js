3fa4331f4e19590b2c5e521a9bc8359095ae9ef5
Se.Okabe <okabe@nexia.jp>
2015-02-10 17:59:55 +0900

    add func request

--- test.js
+++ test.js
@@ -1,6 +1,17 @@
 var log     = console.log;
+var errlog  = console.error;
 var fs      = require('fs');
+var request = require('request');
 
 var Conf = JSON.parse( fs.readFileSync( __dirname + '/.Config.json', 'UTF-8' ) );
 
-log(JSON.stringify(Conf, null, "    "));
+request(Conf.Ping[0].JsonURL, function (err, res, body) {
+  if (err || res.statusCode != 200) {
+  	errlog("Error: " + err);
+  	errlog("Status: %d", res.statusCode);
+  }else{
+  	log(body);
+  }
+});
+  
+//log(JSON.stringify(Conf, null, "    "));
